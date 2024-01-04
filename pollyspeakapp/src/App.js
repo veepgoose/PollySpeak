7fa1f480997fcdd6b758f381a04b26a477d9c6bb
Goose <victoriaplatt@loveplanes.co.uk>
2024-01-04 14:28:43 +0000

    Install Browser Router and setup routes for Login > Dashboard > SqwarkBoard

--- pollyspeakapp/src/App.js
+++ pollyspeakapp/src/App.js
@@ -1,15 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import logo from './Assets/PollySpeakLogo.png';
 import './App.css';
 import Login from './Components/Login/Login';
+import Dashboard from './Components/Dashboard/Dashboard';
+import SqwarkBoard from './Components/SqwarkBoard/SqwarkBoard';
+
 
 function App() {
   return (
+    <BrowserRouter>
+    <Routes>
+    <Route path="/" element={
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Login/>
       </header>
     </div>
+    } />
+    
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/Dashboard" element={<Dashboard/>} />
+        <Route path="/SqwarkBoard" element={<SqwarkBoard/>} />
+   
+    </Routes>
+    </BrowserRouter>
   );
 }
 
