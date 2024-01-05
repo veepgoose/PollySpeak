136780160ffe0b4e2da48a76f7713266ff4f16bb
veepgoose <victoriaplatt@loveplanes.co.uk>
2024-01-05 12:58:59 +0000

    Add SqwarkBoard section to Dashboard, add Profile Icons and text, basic styling

--- pollyspeakapp/src/Components/Dashboard/Dashboard.js
+++ pollyspeakapp/src/Components/Dashboard/Dashboard.js
@@ -1,12 +1,33 @@
 import './Dashboard.css';
 import logo from '../../Assets/PollySpeakLogo.png';
+import ParrotIcon from '../../Assets/ParrotIcon.png';
 
 
 const Dashboard = () => {
     return(
         <div className="Dashboard-Header">
         <img src={logo} className="Dashboard-logo" alt="logo" />
+        <h1 className="SqwarkBoards">SQWARKBOARDS</h1>
+
+        <div className="Dashboard-body">
+            <div className="Profile1">
+            <img src={ParrotIcon} className="Parrot-Icon" alt="logo" />
+            <h1 className="ProfileName">YOSHI</h1>
+            </div>
+             <div className="Profile1">
+            <img src={ParrotIcon} className="Parrot-Icon" alt="logo" />
+            <h1 className="ProfileName">BUZZ</h1>
+            </div>
+            <div className="Profile1">
+            <img src={ParrotIcon} className="Parrot-Icon" alt="logo" />
+            <h1 className="ProfileName">OSCAR</h1>
+            </div>
+
         </div>
+        </div>
+
+     
+       
 
     
     );
