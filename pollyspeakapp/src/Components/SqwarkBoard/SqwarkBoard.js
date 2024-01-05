df4a525d11ad8e2c420f9065ea64158b64e6898c
Goose <victoriaplatt@loveplanes.co.uk>
2024-01-05 18:04:22 +0000

    Add buttons to SqwarkBoard

--- pollyspeakapp/src/Components/SqwarkBoard/SqwarkBoard.js
+++ pollyspeakapp/src/Components/SqwarkBoard/SqwarkBoard.js
@@ -6,6 +6,38 @@ const SqwarkBoard = () => {
     return (
         <div className= "SqwarkBoard-Header">
             <img src={logo} className="Polly-logo" alt="logo" />
+
+        <div className= "SqwarkBoard">
+        <button className="Sqwark-Button" aria-label="Cashew" >
+         <span className="Sqwark-ButtonLabel">Cashew</span>
+        </button>
+        <button className="Sqwark-Button" aria-label="Cashew" >
+         <span className="Sqwark-ButtonLabel">Cashew</span>
+        </button>
+        <button className="Sqwark-Button" aria-label="Cashew" >
+         <span className="Sqwark-ButtonLabel">Cashew</span>
+        </button>
+        <button className="Sqwark-Button" aria-label="Cashew" >
+         <span className="Sqwark-ButtonLabel">Cashew</span>
+        </button>
+        <button className="Sqwark-Button" aria-label="Cashew" >
+         <span className="Sqwark-ButtonLabel">Cashew</span>
+        </button>
+        <button className="Sqwark-Button" aria-label="Cashew" >
+         <span className="Sqwark-ButtonLabel">Cashew</span>
+        </button>
+        <button className="Sqwark-Button" aria-label="Cashew" >
+         <span className="Sqwark-ButtonLabel">Cashew</span>
+        </button>
+        <button className="Sqwark-Button" aria-label="Cashew" >
+         <span className="Sqwark-ButtonLabel">Cashew</span>
+        </button>
+        <button className="Sqwark-Button" aria-label="Cashew" >
+         <span className="Sqwark-ButtonLabel">Cashew</span>
+        </button>
+
+
+        </div>
         </div>
     )
 }
