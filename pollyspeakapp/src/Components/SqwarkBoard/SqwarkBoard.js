c190ee9baee86912f81fe9de8d37ad8463f86c1a
veepgoose <victoriaplatt@loveplanes.co.uk>
2024-01-07 09:38:15 +0000

    Style buttons on SqwarkBoard to create Communication Board layout

--- pollyspeakapp/src/Components/SqwarkBoard/SqwarkBoard.js
+++ pollyspeakapp/src/Components/SqwarkBoard/SqwarkBoard.js
@@ -12,28 +12,28 @@ const SqwarkBoard = () => {
          <span className="Sqwark-ButtonLabel">Cashew</span>
         </button>
         <button className="Sqwark-Button" aria-label="Cashew" >
-         <span className="Sqwark-ButtonLabel">Cashew</span>
+         <span className="Sqwark-ButtonLabel">Pepper</span>
         </button>
         <button className="Sqwark-Button" aria-label="Cashew" >
-         <span className="Sqwark-ButtonLabel">Cashew</span>
+         <span className="Sqwark-ButtonLabel">Sweetcorn</span>
         </button>
         <button className="Sqwark-Button" aria-label="Cashew" >
-         <span className="Sqwark-ButtonLabel">Cashew</span>
+         <span className="Sqwark-ButtonLabel">Walnut</span>
         </button>
         <button className="Sqwark-Button" aria-label="Cashew" >
-         <span className="Sqwark-ButtonLabel">Cashew</span>
+         <span className="Sqwark-ButtonLabel">Orange</span>
         </button>
         <button className="Sqwark-Button" aria-label="Cashew" >
-         <span className="Sqwark-ButtonLabel">Cashew</span>
+         <span className="Sqwark-ButtonLabel">Chickpea</span>
         </button>
         <button className="Sqwark-Button" aria-label="Cashew" >
-         <span className="Sqwark-ButtonLabel">Cashew</span>
+         <span className="Sqwark-ButtonLabel">Monkey Nut</span>
         </button>
         <button className="Sqwark-Button" aria-label="Cashew" >
-         <span className="Sqwark-ButtonLabel">Cashew</span>
+         <span className="Sqwark-ButtonLabel">Grape</span>
         </button>
         <button className="Sqwark-Button" aria-label="Cashew" >
-         <span className="Sqwark-ButtonLabel">Cashew</span>
+         <span className="Sqwark-ButtonLabel">Cucumber</span>
         </button>
 
 
